fa71495bc1c1c844bf2c7c31c2a854eb6488e749
kazz187 <kazzone87@gmail.com>
2019-12-20 01:30:22 +0900

    Refactor generate sub command

--- cmd/fargate-td/cmd/generate.go
+++ cmd/fargate-td/cmd/generate.go
@@ -55,50 +55,64 @@ func (r *GenerateRunner) preRunE(c *cobra.Command, args []string) error {
 }
 
 func (r *GenerateRunner) runE(c *cobra.Command, args []string) error {
-	vars, err := r.VariablesRunner.LoadVariables()
+	taskStr, err := r.GenerateTaskDefinition()
 	if err != nil {
 		return err
 	}
+	fmt.Print(taskStr)
+	return nil
+}
 
+func (r *GenerateRunner) GenerateTaskDefinition() (string, error) {
+	vars, err := r.VariablesRunner.LoadVariables()
+	if err != nil {
+		return "", err
+	}
+
+	// Load overlay task files
 	taskRootPath := r.ProjectRootPath + "/" + taskPath
 	loader := overlay.NewLoader(taskRootPath, r.TargetTaskPath)
 	task, err := loader.LoadOverlayTarget(r.TaskName, vars)
 	if err != nil {
-		return fmt.Errorf("failed to load task files %s: %w", r.TaskName, err)
+		return "", fmt.Errorf("failed to load task files %s: %w", r.TaskName, err)
 	}
+
+	// Create container loader
 	containerRootPath := r.ProjectRootPath + "/" + containerPath
 	cl := overlay.NewContainerLoader(containerRootPath, vars)
 	if task == nil || task.YNode().Kind != yaml.MappingNode {
-		return errors.New("task is not map")
+		return "", errors.New("task is not map")
 	}
+	// Load list of containers
 	cd := task.Field("containerDefinitions")
 	if cd.Value == nil || cd.Value.YNode().Kind != yaml.SequenceNode {
-		return errors.New("containerDefinition is not list")
+		return "", errors.New("containerDefinition is not list")
 	}
 	err = cd.Value.VisitElements(func(conDef *yaml.RNode) error {
 		if conDef == nil || conDef.YNode().Kind != yaml.MappingNode {
 			return nil
 		}
+		// Load container template, if template field is defined
 		conName := conDef.Field("template").Value.YNode().Value
-		c, err := cl.LoadContainer(conName)
+		con, err := cl.LoadContainer(conName)
 		if err != nil {
 			return fmt.Errorf("failed to load container definition: %w", err)
 		}
-		conDef.SetYNode(c.YNode())
+		// Replace template field to container definition
+		conDef.SetYNode(con.YNode())
 		return nil
 	})
 	if err != nil {
-		return fmt.Errorf("failed to load container: %w", err)
+		return "", fmt.Errorf("failed to load container: %w", err)
 	}
 	varsStr, err := vars.String()
 	if err != nil {
-		return fmt.Errorf("failed to convert yaml to string: %w", err)
+		return "", fmt.Errorf("failed to convert yaml to string: %w", err)
 	}
 	taskStr, err := task.String()
 	if err != nil {
-		return fmt.Errorf("failed to convert yaml to string: %w", err)
+		return "", fmt.Errorf("failed to convert yaml to string: %w", err)
 	}
 	logrus.Debugln("generated variables:", varsStr)
-	fmt.Print(taskStr)
-	return nil
+	return taskStr, nil
 }
