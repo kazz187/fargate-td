3a9fe0426822ae0100d93aa73a1060935940e680
kazz187 <kazzone87@gmail.com>
2019-12-24 18:17:18 +0900

    Change build image of Circle CI

--- cmd/fargate-td/cmd/fargate-td.go
+++ cmd/fargate-td/cmd/fargate-td.go
@@ -17,7 +17,7 @@ func NewFargateTdCommand() *cobra.Command {
 		Use:              "fargate-td",
 		Short:            "Manage task definition",
 		Long:             "Manage task definition",
-		Version:          "v0.0.5",
+		Version:          "v0.0.6",
 		PersistentPreRun: ftr.preRun,
 	}
 	root.AddCommand(VariablesCommand(&ftr))
